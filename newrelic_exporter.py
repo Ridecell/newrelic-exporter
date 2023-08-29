de8e244ea55b684a1162653c81deeba5dae47ec7
Gaurang Kudale <gaurang.kudale02@gmail.com>
2023-08-30 04:43:02 +0530

    Getting Throughtput ,error_rate and response time correct

--- newrelic_exporter.py
+++ newrelic_exporter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
+import json
 import time
 import requests
 import click
-import json
+import os
 
 from datetime import datetime, timedelta
 from prometheus_client import start_http_server
@@ -11,25 +12,21 @@ from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
 
 class NewrelicCollector(object):
-  def __init__(self, apikey):
+  def __init__(self, apikey, account_number):
     self.graphql_base_url = "https://api.newrelic.com/graphql"
     self.api_key = apikey
+    self.account_number = account_number
 
   def collect(self):
     print("Collecting\n")
     
     # The metrics we want to export.
     metrics = {
+        # add the metrics for apdex_target
         'apdexScore': GaugeMetricFamily('newrelic_application_apdex_score',' newrelic application apdex_score', labels=["appname"]),
         'errorRate': GaugeMetricFamily('newrelic_application_error_rate',' newrelic application error_rate', labels=["appname"]),
-        'hostCount': GaugeMetricFamily('newrelic_application_host_count','newrelic application host count', labels=["appname"]),
-        'instanceCount': GaugeMetricFamily('newrelic_applicaiton_instance_count','newrelic application instance count', labels=["appname"]),
-        'nonWebResponseTimeAverage': GaugeMetricFamily('newrelic_application_non_web_response_time_average','newrelica applicationnon web response time average',labels=["appname"]),
-        'nonWebThroughput': GaugeMetricFamily('newrelic_application_non_web_throughput','newrelic applicationn on web throughput', labels=["appname"]),
-        'responseTimeAverage': GaugeMetricFamily('newrelic_application_response_time',' newrelic application response in sec', labels=["appname"]),
-        'throughput': GaugeMetricFamily('newrelic_application_throughput',' newrelic application throughput',labels=["appname"]),
-        'webResponseTimeAverage': GaugeMetricFamily('newrelic_application_web_response_time_average','newrelic_application web response time average', labels=["appname"]),
-        'webThroughput': GaugeMetricFamily('newrelic_application_web_throughput','newrelic application web throughput', labels=["appname"]),
+        'webResponseTimeAverage': GaugeMetricFamily('newrelic_application_response_time','newrelic_application web response time average', labels=["appname"]),
+        'webThroughput': GaugeMetricFamily('newrelic_application_throughput','newrelic application web throughput', labels=["appname"]),
     }
 
     # Get all entity Guids
@@ -38,56 +35,51 @@ class NewrelicCollector(object):
     if resp.json().get("errors"):
       print("Error getting newrelic entities:", resp.json())
       return
-    
-    guidDict = {}
-    # saving guid and name in dictionary (guidDict)
-    for entity in resp.json().get("data").get("actor").get("entitySearch").get("results").get("entities"):
-      guidDict[entity["guid"]] = entity["name"]
-
     # Pass the metrics to the prometheus by looping through entity
     for metric in metrics:
       for entity in  resp.json().get("data").get("actor").get("entitySearch").get("results").get("entities"):
         if entity.get("apmSummary"):
           try:
-            metrics[metric].add_metric([entity["name"]], entity.get("apmSummary")[metric])
+            metric_value = entity.get("apmSummary")[metric]
+            if metric == 'webResponseTimeAverage':
+              # Currently apdex_score is in seconds format 
+              # convert apdex_score from seconds to miliseconds
+              metric_value = metric_value * 1000
+            metrics[metric].add_metric([entity["name"]], metric_value)
           except KeyError:
             pass
       yield metrics[metric]
 
-    # We can pass only 25 Guids in one request, so dividing all guids in chunks of 25,
-    # and fetching deployments for those 25 guids
-    # Get list of deployment which happened in last 1 hour
-    t = datetime.today()-timedelta(hours=1)
-    deploymentArray = []
-    guids = list(guidDict.keys())
     
-    chunk_size = 25
-    for i in range(0, len(guids), chunk_size):      
-      resp  = requests.post(url=self.graphql_base_url, headers=headers, data='{{actor {{entities(guids: {0}) {{deploymentSearch(filter: {{timeWindow: {{startTime: {1} }}}}) {{results {{version timestamp entityGuid}}}}}}}}}}'.format(json.dumps(guids[i:i+chunk_size]),round(t.timestamp()*1000)))
+    # Get list of deployment which happened in last 1 hour
+    # Fetch the account number from the environment variable
+    deploymentArray = []
+    newrelic_account_number = int(self.account_number)
+    resp  = requests.post(url=self.graphql_base_url, headers=headers, data='{{actor{{nrql(query:"SELECT * FROM Deployment SINCE 3600 seconds AGO "accounts:{0}){{nrql results}}}}}}'.format(json.dumps(newrelic_account_number)))
+    
+    # Iterate through the deployment results and extract relevant information
+    for deployments in resp.json().get("data").get("actor").get("nrql").get("results"):
       if resp.json().get("errors"):
         print("Error getting newrelic deployments:", resp.json())
         return
-
-      for entity in resp.json().get("data").get("actor").get("entities"):        
-        for deployments in entity.get("deploymentSearch").get("results"):          
-          deploymentArray.append({
-            "name": guidDict.get(deployments.get("entityGuid")),
-            "timestamp": deployments.get("timestamp"),
-            "version": deployments.get("version")
-          })
-        
-    print(deploymentArray)
-    # Expose deployment metric
+      deploymentArray.append({
+        "name": deployments.get("entity.name"),
+        "timestamp": deployments.get("timestamp"),
+        "version": deployments.get("version")
+      })
+    
     deploymentMetric = GaugeMetricFamily('newrelic_application_deployment',' newrelic application deployment', labels=["appname", "version"])
     for deployment in deploymentArray:
       # Passing timestamp as seconds because prometheus client automatically converts timestamp to milliseconds
       deploymentMetric.add_metric([deployment['name'], deployment['version']], 1, int(deployment['timestamp']/1000))
     yield deploymentMetric
-
+    
 @click.command()
 @click.option('--api-key', '-a', envvar='APIKEY', help='API key for newrelic', required=True)
-def main(api_key):
-  REGISTRY.register(NewrelicCollector(api_key))
+@click.option('--account-number','-n',envvar='NEWRELIC_ACCOUNT_NUMBER', help='Account Number for newrelic',required=True)
+def main(api_key, account_number):   
+  collector = NewrelicCollector(api_key, account_number)
+  REGISTRY.register(collector)
   start_http_server(9126)
   while True: 
     time.sleep(1)

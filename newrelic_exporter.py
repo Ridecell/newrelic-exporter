543e8ee561969af57b9b1ead3429c02b172a4945
Gaurang Kudale <gaurang.kudale02@gmail.com>
2023-09-01 12:18:54 +0530

    added new metrics NonWebThroughput, NonWebResponseTime and Restructured query

--- newrelic_exporter.py
+++ newrelic_exporter.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import json
 import time
 import requests
 import click
@@ -20,14 +19,37 @@ class NewrelicCollector(object):
     # The metrics we want to export.
     metrics = {
         # add the metrics
-        'apdexScore': GaugeMetricFamily('newrelic_application_apdex_score',' newrelic application apdex_score', labels=["appname"]),
         'errorRate': GaugeMetricFamily('newrelic_application_error_rate',' newrelic application error_rate', labels=["appname"]),
         'webResponseTimeAverage': GaugeMetricFamily('newrelic_application_response_time','newrelic_application web response time average', labels=["appname"]),
         'webThroughput': GaugeMetricFamily('newrelic_application_throughput','newrelic application web throughput', labels=["appname"]),
-    }
+        'nonWebThroughput': GaugeMetricFamily('newrelic_nonweb_throughput','newrelic applicaiotn non web throughput', labels=["appname"]),
+        'nonWebResponseTimeAverage': GaugeMetricFamily('newrelic_nonweb_response_time_average','newrelic nonweb response time average', labels=["appname"]),
+   }
 
     headers = {'API-Key': self.api_key}
-    resp  = requests.post(url=self.graphql_base_url, headers=headers, data="{actor {entitySearch(queryBuilder: {domain: APM}) {results {entities { ... on ApmApplicationEntityOutline {name apmSummary { apdexScore errorRate webResponseTimeAverage webThroughput}}}}}}}")
+
+    graphQuery = """{
+                        actor {
+                          entitySearch(queryBuilder: {domain: APM}) {
+                            results {
+                              entities {
+                              ... on ApmApplicationEntityOutline {
+                                    name
+                                    apmSummary {
+                                    errorRate
+                                    webResponseTimeAverage
+                                    webThroughput
+                                    nonWebThroughput
+                                    nonWebResponseTimeAverage
+                                  }
+                               }
+                             }
+                          }
+                        }
+                      }
+                    } """
+
+    resp  = requests.post(url=self.graphql_base_url, headers=headers, data=graphQuery)
     if resp.json().get("errors"):
       print("Error getting newrelic entities:", resp.json())
       return
@@ -49,7 +71,20 @@ class NewrelicCollector(object):
     deploymentMetric = GaugeMetricFamily('newrelic_application_deployment',' newrelic application deployment', labels=["appname", "version"])
     
     timeInSeconds = 3600  # Get list of deployment which happened in last 1 hour
-    resp  = requests.post(url=self.graphql_base_url, headers=headers, data='{{actor{{nrql(query:"SELECT * FROM Deployment SINCE {0} seconds AGO "accounts:{1}){{nrql results}}}}}}'.format(timeInSeconds,self.account_number))
+
+    deploymentGraphQuery = """{{
+                                actor{{
+                                  nrql(
+                                    query: "SELECT * FROM Deployment SINCE {0} seconds AGO "
+                                    accounts: {1}
+                                  ) {{
+                                      nrql 
+                                      results
+                                      }}
+                                  }}
+                              }}"""
+    resp  = requests.post(url=self.graphql_base_url, headers=headers, data=deploymentGraphQuery.format(timeInSeconds,self.account_number))
+    
     if resp.json().get("errors"):
         print("Error getting newrelic deployment response : ", resp.json())
         return
